88080e4cbd2f17441b7ccd11677492135e32a6af
Dzemidovich <andrewdzemidovich@gmail.com>
2024-11-18 20:41:10 +0300

    Dzemidovich

---
+++ 8kyu№1/Program.cs
@@ -0,0 +1,17 @@
+﻿public static class Kata
+{
+    public static object[] RemoveEveryOther(object[] arr)
+    {
+
+        List<object> result = new List<object>();
+
+        // Перебираем массив и добавляем только элементы с четным индексом (0, 2, 4...)
+        for (int i = 0; i < arr.Length; i += 2)
+        {
+            result.Add(arr[i]);
+        }
+
+        // Преобразуем список обратно в массив и возвращаем
+        return result.ToArray();
+    }
+}
