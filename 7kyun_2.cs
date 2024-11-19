682dc6156d1163d2531828156d5f8e5621c6dcd6
Andrew <andrewdzemidovich@gmail.com>
2024-11-19 16:29:58 +0300

    Add files via upload
    добавление 7-го лвл

---
+++ 7kyun_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string Disemvowel(string str)
+    {
+        // Задаем массив гласных, которые нужно удалить
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+
+        foreach (char vowel in vowels)
+        {
+            str = str.Replace(vowel.ToString(), ""); // Заменяем каждую гласную на пустую строку
+        }
+
+        return str;
+    }
+}
---
+++ 7kyun_3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Kata
+{
+    public static int SquareDigits(int n)
+    {
+        
+        string result = "";
+
+        foreach (char digit in n.ToString())
+        {
+           
+            result += (int)Math.Pow(char.GetNumericValue(digit), 2);
+        }
+
+        return int.Parse(result);
+    }
+}
