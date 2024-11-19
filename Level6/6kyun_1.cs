998177ac79de5fcdd6d822b8a589f185910cca1a
Andrew <andrewdzemidovich@gmail.com>
2024-11-19 18:43:11 +0300

    Add files via upload
    добавление задачек

---
+++ Level6/6kyun_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+public static class Kata
+{
+    public static string ExpandedForm(long num)
+    {
+        string numberStr = num.ToString();
+        int length = numberStr.Length;
+        StringBuilder result = new StringBuilder(); 
+        for (int i = 0; i < length; i++)
+        {
+            if (numberStr[i] != '0') 
+            {
+                long value = (numberStr[i] - '0') * (long)Math.Pow(10, length - i - 1);
+
+                if (result.Length > 0)
+                {
+                    result.Append(" + ");
+                }
+                result.Append(value); // Добавляем значение
+            }
+        }
+
+        return result.ToString(); 
+    }
+}
---
+++ Level6/6kyun_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Kata
+{
+    public static int Solve(string s)
+    {
+        const string vowels = "aeiou"; // Строка гласных
+        int maxSum = 0;
+        int currentSum = 0;
+
+        foreach (char c in s)
+        {
+            if (vowels.IndexOf(c) == -1) 
+            {
+                currentSum += c - 'a' + 1; 
+                maxSum = Math.Max(maxSum, currentSum); // Обновляем максимальную сумму
+            }
+            else
+            {
+                currentSum = 0; // Сброс текущей суммы при встрече гласной
+            }
+        }
+
+        return maxSum;
+    }
+}
