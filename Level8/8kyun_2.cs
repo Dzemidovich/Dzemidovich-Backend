287e91de8e0bc7a1f06a1e1a3281480767e4c794
Andrew <andrewdzemidovich@gmail.com>
2024-11-19 16:50:58 +0300

    Add files via upload
    добавление задачек

---
+++ Level8/8kyun_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int Enough(int cap, int on, int wait)
+    {
+        // Рассчитываем, сколько мест останется на автобусе
+        int availableSpace = cap - on;
+
+        // Если доступного места достаточно для всех ожидающих
+        if (availableSpace >= wait)
+        {
+            return 0; // Все могут сесть
+        }
+
+        // Возвращаем количество пассажиров, которые не смогут сесть
+        return wait - availableSpace;
+    }
+}
